bafe6906bf02e3e9e2f7a3b97d454f764084341c
George <georgericardo26@gmail.com>
2019-08-17 17:20:11 +0000

    Update url_user.py

--- main/api/v1/urls/url_user.py
+++ main/api/v1/urls/url_user.py
@@ -7,18 +7,6 @@ urlpatterns = [
     path('list',
          views.UserList.as_view(),
          name='user_list'),
-     
-    # path('<int:pk>/',
-    #      views.UserView.as_view(),
-    #      name='user_retrieve_update_destroy_destroy_by_pk'),
-
-    # path('<slug:username>/',
-    #      views.UserView.as_view(),
-    #      name='user_retrieve_update_destroy_by_username'),
-
-    # re_path(r'^(?P<email>[\w.@+-]+)/$',
-    #         views.UserView.as_view(),
-    #         name='user_retrieve_update_destroy_by_email'),
 
     path('alterpassword/<int:pk>/',
          views.UserAlterPasswordView.as_view(),
