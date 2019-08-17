50e70a571dc91b93c90ee063fbae40c3a2a9ccc1
George <georgericardo26@gmail.com>
2019-08-17 17:12:07 +0000

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,7 +6,6 @@ from django.conf import settings
 
 app_name = "project"
 urlpatterns = [
-    # path('api/', include('core.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('admin/', admin.site.urls, name="admin_page"),
     path('api/', include('main.urls'))
